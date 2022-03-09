b8443389f42ce970e3c0f068bf26693d57736a09
van860104 <janess114060402@gmail.com>
2022-03-09 18:56:29 +0800

    frist commit

--- AI_Carrefour/ChatViewController.swift
+++ AI_Carrefour/ChatViewController.swift
@@ -10,29 +10,55 @@ import UIKit
 
 
 class ChatViewController: UIViewController {
-  
     
+    
+    
+    @IBOutlet weak var userinfo: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-  
+      	
+        //        userinfo.contentInset = UIEdgeInsets(top: 12, left: 5, bottom: 12, right: 5)
         
+        userinfo.isEditable = false
+        userinfo.isUserInteractionEnabled = false
+        userinfo.layer.cornerRadius = 20
+        userinfo.clipsToBounds = true
         
+        userinfo.text = "您搜尋的商品為： "
         
-        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var buttonsStack: UIStackView!
+
+
+
+
+    
+    var i = 0
+    @IBAction func test(_ sender: Any) {
+         
+        
+                   let x:Int = 100 + (i % 4) * 60
+                   let y:Int = 140 + (i / 4) * 60
+            let buttonNumber:UIButton = UIButton(type: UIButton.ButtonType.system) as UIButton
+                   //按鈕位置 大小
+                   buttonNumber.frame = CGRect(x: x,y: y,width: 40, height: 35)
+                   //文字顏色
+            buttonNumber.setTitleColor(UIColor.white, for: UIControl.State.normal)
+                   //按鈕背景
+                   buttonNumber.backgroundColor = UIColor.black
+                   //字型大小
+                   buttonNumber.titleLabel?.font = UIFont(name: "System", size: 22.0)
+                   //加入按鈕
+                   view.addSubview(buttonNumber)
+               
+          i = i+1
+          
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
     @IBAction func goback(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
